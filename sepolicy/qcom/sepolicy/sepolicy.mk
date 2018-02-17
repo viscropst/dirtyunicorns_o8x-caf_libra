794b3db69147f4a5d66c290860d1d88e1cbe7c57
viscropst <bcct2010@live.cn>
2018-02-17 19:56:09 +0800

    sepolicy/qcom:Fix typo

--- sepolicy/qcom/sepolicy/sepolicy.mk
+++ sepolicy/qcom/sepolicy/sepolicy.mk
@@ -1,16 +1,16 @@
 # Board specific SELinux policy variable definitions
 BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/common \
-    device/qcom/sepolicy/ssg \
-    device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+    device/xiaomi/libra/sepolicy/qcom/sepolicy/common \
+    device/xiaomi/libra/sepolicy/qcom/sepolicy/ssg \
+    device/xiaomi/libra/sepolicy/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/test
+    device/xiaomi/libra/sepolicy/qcom/sepolicy/test
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/public
+    device/xiaomi/libra/sepolicy/qcom/sepolicy/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy/private
+    device/xiaomi/libra/sepolicy/qcom/sepolicy/private
